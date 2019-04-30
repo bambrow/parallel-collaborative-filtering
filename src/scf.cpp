9369b6ef83dd7f38d7d2fa8068144b8bccb86d4f
bambrow <bambrow@icloud.com>
2019-04-30 04:24:51 -0400

    update serial algorithm

--- src/scf.cpp
+++ src/scf.cpp
@@ -11,6 +11,9 @@
 
 using namespace std;
 
+void scf_user(const int& num_users, const int& num_items, float** utility, float** filled_utility);
+void scf_item(const int& num_users, const int& num_items, float** utility, float** filled_utility);
+
 return_code scf(string filename, bool item_option) 
 {
     ifstream in(filename);
@@ -91,6 +94,8 @@ return_code scf(string filename, bool item_option)
     }
     #endif
 
+    float* user_average = new float[num_users];
+
     float missing_rating = (float) MISSING_RATING;
 
     for (int i = 0; i < num_users; i++) {
@@ -102,15 +107,21 @@ return_code scf(string filename, bool item_option)
                 user_item_count++;
             }
         }
-        float user_average = user_item_sum / user_item_count;
+        float user_avg = user_item_sum / user_item_count;
+        user_average[i] = user_avg;
         for (int j = 0; j < num_items; j++) {
             if (abs(utility[i][j] - missing_rating) > 1e-3) {
-                utility[i][j] -= user_average;
+                utility[i][j] -= user_avg;
             }
         }
     }
 
     #ifdef DEBUG_1
+    cout << "user average: " << endl;
+    for (int i = 0; i < num_users; i++) {
+        printf("%.2f\t", user_average[i]);
+        cout << endl;
+    }
     cout << "normalized matrix: " << endl;
     for (int i = 0; i < num_users; i++) {
         for (int j = 0; j < num_items; j++) {
@@ -119,6 +130,50 @@ return_code scf(string filename, bool item_option)
         cout << endl;
     }
     #endif
+
+    float** filled_utility = new float*[num_users];
+    for (int i = 0; i < num_users; i++) {
+        filled_utility[i] = new float[num_items];
+    }
+
+    if (!item_option) {
+        scf_user(num_users, num_items, utility, filled_utility);
+    } else {
+        scf_item(num_users, num_items, utility, filled_utility);
+    }
+
+    #ifdef DEBUG_1
+        cout << "filled normalized matrix: " << endl;
+        for (int i = 0; i < num_users; i++) {
+            for (int j = 0; j < num_items; j++) {
+                printf("%.2f\t", filled_utility[i][j]);
+            }
+            cout << endl;
+        }
+    #endif
+
+    for (int i = 0; i < num_users; i++) {
+        for (int j = 0; j < num_items; j++) {
+            filled_utility[i][j] += user_average[i];
+        }
+    }
+
+    #ifdef DEBUG_1
+        cout << "filled original matrix: " << endl;
+        for (int i = 0; i < num_users; i++) {
+            for (int j = 0; j < num_items; j++) {
+                printf("%.2f\t", filled_utility[i][j]);
+            }
+            cout << endl;
+        }
+    #endif
+
+    return SUCCESS;
+}
+
+void scf_user(const int& num_users, const int& num_items, float** utility, float** filled_utility) 
+{
+    float missing_rating = (float) MISSING_RATING;
 
     float* user_norm = new float[num_users];
 
@@ -171,15 +226,9 @@ return_code scf(string filename, bool item_option)
 //    to fill the missing value in the matrix
     unsigned num_most_similar = NUM_MOST_SIMILAR;
 
-    float** filled_utility = new float*[num_users];
-
     for (int i = 0; i < num_users; i++) {
-        filled_utility[i] = new float[num_items];
-    }
-
-    for (int i = 0; i < num_users; i++){
-        for (int k = 0; k < num_items; k++){
-            if (abs(utility[i][k] - missing_rating) < 1e-3){
+        for (int k = 0; k < num_items; k++) {
+            if (abs(utility[i][k] - missing_rating) < 1e-3) {
                 priority_queue<SimTuple, vector<SimTuple>, CompareSimTuple> pq;
 
                 for (int l = 0; l < num_users; l++) {
@@ -210,22 +259,106 @@ return_code scf(string filename, bool item_option)
                     filled_utility[i][k] = numerator/denominator;
                 }
 
-            }else{
+            } else {
                 filled_utility[i][k] = utility[i][k];
             }
 
         }
     }
+}
+
+void scf_item(const int& num_users, const int& num_items, float** utility, float** filled_utility) 
+{
+    float missing_rating = (float) MISSING_RATING;
+
+    float* item_norm = new float[num_items];
+
+    float** similarity = new float*[num_items];
+    for (int i = 0; i < num_items; i++) {
+        similarity[i] = new float[num_items];
+    }
+
+    for (int i = 0; i < num_items; i++) {
+        float item_square_sum = 0;
+        for (int j = 0; j < num_users; j++) {
+            if (abs(utility[j][i] - missing_rating) > 1e-3) {
+                item_square_sum += utility[j][i] * utility[j][i];
+            }
+        }
+        item_norm[i] = sqrt(item_square_sum);
+    }
 
     #ifdef DEBUG_1
-        cout << "filled normalized matrix: " << endl;
-        for (int i = 0; i < num_users; i++) {
-            for (int j = 0; j < num_items; j++) {
-                printf("%.2f\t", filled_utility[i][j]);
-            }
-            cout << endl;
-        }
+    cout << "rating norm per item: " << endl;
+    for (int i = 0; i < num_items; i++) {
+        printf("%.2f\t", item_norm[i]);
+        cout << endl;
+    }
     #endif
 
-    return SUCCESS;
+    for (int i = 0; i < num_items; i++) {
+        for (int j = 0; j < i; j++) {
+            float temp_cosine = 0;
+            for (int k = 0; k < num_users; k++) {
+                if (abs(utility[k][i] - missing_rating) > 1e-3 && abs(utility[k][j] - missing_rating) > 1e-3) {
+                    temp_cosine += utility[k][i] * utility[k][j];
+                }
+            }
+            similarity[i][j] = similarity[j][i] = temp_cosine / (item_norm[i] * item_norm[j]);
+        }
+        similarity[i][i] = 1;
+    }
+
+    #ifdef DEBUG_1
+    cout << "similarity matrix: " << endl;
+    for (int i = 0; i < num_items; i++) {
+        for (int j = 0; j < num_items; j++) {
+            printf("%.2f\t", similarity[i][j]);
+        }
+        cout << endl;
+    }
+    #endif
+
+//    to fill the missing value in the matrix
+    unsigned num_most_similar = NUM_MOST_SIMILAR;
+
+    for (int i = 0; i < num_items; i++) {
+        for (int k = 0; k < num_users; k++) {
+            if (abs(utility[k][i] - missing_rating) < 1e-3){
+                priority_queue<SimTuple, vector<SimTuple>, CompareSimTuple> pq;
+
+                for (int l = 0; l < num_items; l++) {
+                    if (l == i) {
+                        continue;
+                    }
+                    if (abs(utility[k][l] - missing_rating) > 1e-3) {
+
+                        pq.push(SimTuple(l, similarity[i][l]));
+
+                        if (pq.size() == num_most_similar + 1) {
+                            pq.pop();
+                        }
+                    }
+                }
+
+                float denominator = 0;
+                float numerator = 0;
+
+                while (!pq.empty()) {
+                    int item = pq.top().id;
+                    pq.pop();
+
+                    denominator += abs(similarity[i][item]);
+                    numerator += utility[k][item] * similarity[i][item];
+                }
+                if (denominator != 0) {
+                    filled_utility[k][i] = numerator/denominator;
+                }
+
+            } else {
+                filled_utility[k][i] = utility[k][i];
+            }
+
+        }
+    }
 }
